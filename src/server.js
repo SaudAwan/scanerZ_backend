3f719eb5e1895c0c8ff5bb08ad29b57a634f667c
ikaltaurux <ilyaskhan@altaurux.com>
2023-02-06 17:37:44 +0500

    updating server.js file
    adding cookieParser
    removing test api
    updating port

--- src/server.js
+++ src/server.js
@@ -4,22 +4,20 @@ const bodyParser = require('body-parser')
 const connectToMongoDB = require('./config/connectDB.config')
 const routes = require('./routes')
 const cors = require('cors')
+const cookieParser = require('cookie-parser')
 
 const app = express()
 
 app.use(cors())
+app.use(cookieParser())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-
-app.get('/', (_, res) => {
-	res.send('Welcome to the API')
-})
 
 app.use('/api/v1', routes)
 
 const startServer = async () => {
 	try {
-		const PORT = process.env.PORT
+		const PORT = process.env.PORT || 5002
 		await connectToMongoDB()
 		app.listen(PORT, () => {
 			console.log(`server started on port http://localhost:${PORT}`)
