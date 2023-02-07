6d023fbf303d9dd63c57e61beaf63bc4fa6bacaf
ikaltaurux <ilyaskhan@altaurux.com>
2023-02-07 17:27:45 +0500

    update verifyToken

--- src/utils/verifyToken.utils.js
+++ src/utils/verifyToken.utils.js
@@ -1,10 +1,12 @@
 const jwt = require('jsonwebtoken')
 const createError = require('./createError.utils.js')
 
-const verifyToken = (token) => {
-	if (!token) return next(createError(401, 'You are not athenticated'))
+const verifyToken = (header) => {
+	if (!header) return createError(401, 'Please provide a token')
+	const token = header.split(' ')[1]
+	if (!token) return createError(401, 'You are not athenticated')
 	const { _id } = jwt.verify(token, process.env.JWT_SECRET)
-	if (!_id) return next(createError(401, 'Token is not valid'))
+	if (!_id) return createError(401, 'Token is not valid')
 	return _id
 }
 
