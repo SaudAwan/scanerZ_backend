78dc6e39d0ae9ccf328b76a7381c6f1b35ab4bcc
ikaltaurux <ilyaskhan@altaurux.com>
2023-02-07 17:32:58 +0500

    Create auth middleware

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken')
+const STATUS = require('../constant/status.constant')
+const User = require('../models/user.model')
+const createError = require('../utils/createError.utils')
+const verifyToken = require('../utils/verifyToken.utils')
+
+const authMiddleware = async (req, res, next) => {
+	try {
+		const userId = verifyToken(req.headers.authorization)
+		const user = await User.findById(userId)
+		if (!user) return next(createError(STATUS.NOT_FOUND, 'User not found'))
+		req.user = user
+		next()
+	} catch (err) {
+		res.status(STATUS.UNAUTHORIZED).json({
+			status: STATUS.UNAUTHORIZED,
+			message: err.message,
+		})
+	}
+}
+
+module.exports = authMiddleware
