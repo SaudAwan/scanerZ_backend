5e5bc4ecc53d34efdd43a9b781deab430b87644f
ikaltaurux <ilyaskhan@altaurux.com>
2023-02-07 17:29:06 +0500

    add route for files

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const authRoutes = require('./auth.route')
+const fileRoutes = require('./file.route')
 
 router.use('/auth', authRoutes)
+router.use('/file', fileRoutes)
 
 module.exports = router
