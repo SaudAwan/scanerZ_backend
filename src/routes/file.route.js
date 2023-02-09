1d2d8276ecf887e393ca02e0688898bb1197786a
ikaltaurux <ilyaskhan@altaurux.com>
2023-02-09 14:27:42 +0500

    Add route for qrcode and refactor some routes

--- src/routes/file.route.js
+++ src/routes/file.route.js
@@ -4,10 +4,12 @@ const fileController = require('../controllers/file.controller')
 const authMiddleware = require('../middlewares/auth.middleware')
 
 router.post('/upload', authMiddleware, fileController.uploadFile)
+router.get('/qrcode', fileController.generateQRcode)
 router.get('/get-files', fileController.getAllFiles)
+router.get('/get-file-with-formate', fileController.getFileWithFormate)
 router.get('/get-user-files/:id', fileController.getUserFiles)
 router.get('/:id', fileController.getFile)
-router.put('/:id', authMiddleware, fileController.updateFile)
+router.put('/data/:id', authMiddleware, fileController.updateFile)
 router.delete('/:id', authMiddleware, fileController.deleteFile)
 
 module.exports = router
