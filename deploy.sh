92edb849f1c9fcccb411ed2eebc9eb0ee4cce3ba
tariqbrohi143 <tariqbrohi143@gmail.com>
2023-07-17 22:03:38 +0500

    added PORT in .env

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ ssh root@165.22.177.233 << 'ENDSSH'
     cd scanerZ_backend
     eval "$(ssh-agent -s)"
     ssh-add ~/.ssh/id_rsa
+    git stash
     git pull origin main
     pm2 restart server
 ENDSSH
